3514ad80f4a75de211c2c7f42665d7869df0ee82
haseebasim <haseebzaidi123@gmail.com>
2022-09-16 15:04:08 +0500

    project card size changed

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function ProjectCard() {
   return (
-    <div className="w-[375px] card-box-shadow rounded-[20px] bg-white bg-opacity-[0.07]">
-      <div className="relative w-full h-[260px]">
+    <div className="w-[320px] card-box-shadow rounded-[20px] bg-white bg-opacity-[0.07]">
+      <div className="relative w-full h-[220px]">
         <Image
           src={"/images/project-card-img.png"}
           alt="project img"
@@ -12,19 +12,19 @@ export default function ProjectCard() {
       </div>
       <div className="w-full px-[28px] py-[25px] ">
         <div className="w-full flex flex-col gap-y-3">
-          <p className="text-[28px]">Project Tile goes here</p>
+          <p className="text-2xl">Project Title goes here</p>
           <p className="text-base">
             This is sample project description random things are here in
             description This is sample project lorem ipsum generator for dummy
             content
           </p>
-          <p className="text-base">
+          <p className="text-sm">
             <span className="font-semibold">Tech stack :</span> HTML ,
             JavaScript, SASS, React
           </p>
         </div>
         <div className="flex justify-between items-center text-primary mt-5">
-          <a href="" className="flex gap-x-2 items-center">
+          <a href="" className="flex gap-x-2 text-sm items-center">
             <Image
               src={"/icons/link_icon.svg"}
               alt="project img"
@@ -33,7 +33,7 @@ export default function ProjectCard() {
             />
             <span>Live Preview</span>
           </a>
-          <a href="" className="flex gap-x-2 items-center">
+          <a href="" className="flex text-sm gap-x-2 items-center">
             <Image
               src={"/icons/github_link_icon.svg"}
               alt="project img"

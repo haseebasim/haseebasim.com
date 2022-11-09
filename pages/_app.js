eb02c2806864c0d40c265d40cee0ac7251d39c2f
Syed Haseeb Asim Zaidi <45163316+haseebasim@users.noreply.github.com>
2022-11-09 17:18:21 +0500

    stoped tracking env

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(process.env.NEXT_PUBLIC_HJID, process.env.NEXT_PUBLIC_HJSV)
+  }, [])
+
   return <Component {...pageProps} />
 }
 

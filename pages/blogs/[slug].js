50d1f1a93a1301b212412cd239375b3b0017d4ab
Syed Haseeb Asim Zaidi <45163316+haseebasim@users.noreply.github.com>
2023-03-13 20:23:03 +0500

    updated blog component

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -14,47 +14,39 @@ const components = {};
 
 export default function TestPage({ source }) {
   return (
-    source && (
-      <>
-        <Meta title={source.frontmatter.title} route={`/blogs/${source.frontmatter.title}`} />
-        <Nav />
-        <BottomNav />
-        <Section>
-          <div className="relative mb-10 mt-20 xl:mt-0 flex gap-y-2 flex-col">
-            <div className="relative w-full h-96 lg:h-[522px] rounded-md">
-              <Image
-                className="blog-header-img rounded-md"
-                src={source.frontmatter.featuredImage}
-                alt={source.frontmatter.title}
-                layout="fill"
-              />
-              <div className="absolute flex flex-col justify-center items-center inset-0 bg-[#252B2F] bg-opacity-50 w-full h-full ">
-                <div className="sm:w-3/4 lg:w-2/4 px-2 text-center">
-                  <p className="font-semibold text-3xl sm:text-4xl  leading-snug ">
-                    {source.frontmatter.title}
-                  </p>
-                  <p className="mt-6">{source.frontmatter.description}</p>
-                </div>
+    <>
+      <Meta title={source.frontmatter.title} route={`/blogs/${source.frontmatter.title}`} />
+      <Nav />
+      <BottomNav />
+      <Section>
+        <div className="relative mb-10 mt-20 xl:mt-0 flex gap-y-2 flex-col">
+          <div className="relative w-full h-96 lg:h-[522px] rounded-md">
+            <Image
+              className="blog-header-img rounded-md"
+              src={source.frontmatter.featuredImage}
+              alt={source.frontmatter.title}
+              layout="fill"
+            />
+            <div className="absolute flex flex-col justify-center items-center inset-0 bg-[#252B2F] bg-opacity-50 w-full h-full ">
+              <div className="sm:w-3/4 lg:w-2/4 px-2 text-center">
+                <p className="font-semibold text-3xl sm:text-4xl  leading-snug ">
+                  {source.frontmatter.title}
+                </p>
+                <p className="mt-6">{source.frontmatter.description}</p>
               </div>
             </div>
           </div>
-          <div className="markdown-wrapper">
-            <MDXRemote {...source} components={components} />
-          </div>
-        </Section>
-      </>
-    )
+        </div>
+        <div className="markdown-wrapper">
+          <MDXRemote {...source} components={components} />
+        </div>
+      </Section>
+    </>
   );
 }
 
 export async function getStaticPaths() {
   const directoryPath = path.join(process.cwd(), 'util/blogs');
-  if (fs.existsSync(directoryPath))
-    return {
-      paths: [{ params: { slug: 'undefined' } }],
-      fallback: false
-    };
-
   const fileNames = fs.readdirSync(directoryPath);
   const blogList = fileNames.map((fileName) => {
     const slug = fileName.replace(/\.md$/, '');
@@ -70,7 +62,6 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  if (params.slug === 'undefined') return { props: {} };
   const source = (
     await fs.promises.readFile(path.join(process.cwd(), `util/blogs/${params.slug}.md`))
   ).toString();
